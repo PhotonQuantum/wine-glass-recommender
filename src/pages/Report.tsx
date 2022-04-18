8227aa009ed11e789a79e463c73d3b511cdc9ec9
LightQuantum <self@lightquantum.me>
2022-04-18 20:30:03 +0800

    feat(report): adjust font and buttons

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -16,12 +16,13 @@ import {Diamond, GlassFull, Omega, ShoppingCart} from "tabler-icons-react";
 import {useMediaQuery} from "@mantine/hooks";
 import {Categories, glasses} from "../glasses";
 
-const categoryToIcon: (theme: MantineTheme) => { [key in Categories]: JSX.Element } = (theme: MantineTheme) => ({
-    [Categories.crystal]: <Diamond strokeWidth={1}/>,
-    [Categories.red]: <GlassFull color={theme.colors.pink[7]} strokeWidth={1}/>,
-    [Categories.white]: <GlassFull color={theme.colors.lime[2]} strokeWidth={1}/>,
-    [Categories.universal]: <Omega strokeWidth={1}/>
-});
+const categoryToIcon: (theme: MantineTheme) => { [key in Categories]: JSX.Element } =
+    (theme) => ({
+        [Categories.crystal]: <Diamond strokeWidth={1}/>,
+        [Categories.red]: <GlassFull color={theme.colors.pink[7]} strokeWidth={1}/>,
+        [Categories.white]: <GlassFull color={theme.colors.lime[2]} strokeWidth={1}/>,
+        [Categories.universal]: <Omega strokeWidth={1}/>
+    });
 
 export const Report = () => {
     const theme = useMantineTheme();
@@ -31,25 +32,25 @@ export const Report = () => {
             <Stack pt={md ? 128 : 30} px={50} spacing={30} sx={{maxWidth: 1000}}>
                 <Text size={"xl"} weight={300}>您的酒杯推荐</Text>
                 <Box>
-                    <Text size={"md"} weight={300}>根据您的回答，您可能想要这些酒杯</Text>
-                    <Text size={"md"} weight={300}>这些图标会帮助您挑选您的酒杯</Text>
+                    <Text size={"md"}>根据您的回答，您可能想要这些酒杯</Text>
+                    <Text size={"md"}>这些图标会帮助您挑选您的酒杯</Text>
                 </Box>
                 <SimpleGrid cols={md ? 3 : 2} sx={{maxWidth: 350}}>
                     <Group>
                         <GlassFull color={theme.colors.pink[7]} strokeWidth={1}/>
-                        <Text size={"md"} weight={300}>红酒杯</Text>
+                        <Text size={"md"}>红酒杯</Text>
                     </Group>
                     <Group>
                         <GlassFull color={theme.colors.lime[2]} strokeWidth={1}/>
-                        <Text size={"md"} weight={300}>白酒杯</Text>
+                        <Text size={"md"}>白酒杯</Text>
                     </Group>
                     <Group>
                         <Omega strokeWidth={1}/>
-                        <Text size={"md"} weight={300}>通用杯</Text>
+                        <Text size={"md"}>ISO 杯</Text>
                     </Group>
                     <Group>
                         <Diamond strokeWidth={1}/>
-                        <Text size={"md"} weight={300}>水晶杯</Text>
+                        <Text size={"md"}>水晶杯</Text>
                     </Group>
                 </SimpleGrid>
                 <Space/>
@@ -57,22 +58,22 @@ export const Report = () => {
                     {
                         glasses.map((glass) => {
                             return (
-                                <Card withBorder={true}>
+                                <Card withBorder={true} sx={{display: "flex", flexDirection: "column"}}>
                                     <Card.Section>
                                         <Box sx={{backgroundColor: "#ffffff"}}>
                                             <Image src={glass.img.toString()} height={223} fit={"contain"}/>
                                         </Box>
                                     </Card.Section>
-                                    <Stack pt={10}>
+                                    <Stack pt={10} sx={{flexGrow: 1}}>
                                         <Group spacing={5}>
                                             {glass.categories.map(category => (categoryToIcon(theme)[category]))}
                                         </Group>
                                         <Text weight={600}>{glass.name}</Text>
-                                        <Text size={"sm"} weight={200}>
+                                        <Text size={"sm"}>
                                             {glass.description}
                                         </Text>
                                         <ActionIcon<'a'> component={"a"} href={glass.link} target={"_blank"}
-                                                         size={"lg"} radius={"xl"}>
+                                                         size={"lg"} radius={"xl"} mt={"auto"}>
                                             <ShoppingCart strokeWidth={1}/>
                                         </ActionIcon>
                                     </Stack>
